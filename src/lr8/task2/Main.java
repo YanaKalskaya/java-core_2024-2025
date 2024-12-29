bf3a947ad2700e930c5d76d8c121a8749ab703cb
Yana Kalskaya <yaalka2004@gmail.com>
2024-12-29 13:48:16 +0500

    lr8.3

--- src/lr8/task2/Main.java
+++ src/lr8/task2/Main.java
@@ -19,46 +19,28 @@ public class Main {
             System.out.println("Введите первую строку: ");
             String str1 = sc.nextLine();
             wr1.writeUTF(str1);
-            wr1.writeUTF("\n");
 
             System.out.println("Введите вторую строку: ");
             String str2 = sc.nextLine();
             wr1.writeUTF(str2);
-            wr1.writeUTF("\n");
 
             System.out.println("Введите 5 чисел типа double: ");
             for (int i = 0; i < 5; i++) {
                 wr1.writeDouble(sc.nextDouble());
-                wr1.writeUTF("\n");
             }
 
+            //чтение данных из первого файла (text.txt) и запись во второй (text2.txt)
             rd = new DataInputStream(new FileInputStream(f1.getAbsolutePath()));
             wr2 = new DataOutputStream(new FileOutputStream("src/lr8/task2/text2.txt"));
-            System.out.println(rd.readUTF());
+            rd.readUTF();
             wr2.writeUTF(rd.readUTF());
             for (int i = 1; i <= 5; i++) {
                 double num = rd.readDouble();
-                System.out.println(num);
                 if (num > 0) {
+                    System.out.println(num);
                     wr2.writeDouble(num);
-                    wr2.writeUTF("\n");
                 }
             }
-            // Создание потоков для чтения и записи с кодировкой UTF-8
-//            br = new BufferedReader(new InputStreamReader(new FileInputStream("src/lr8/task2/text.txt"), "cp1251"));
-//            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("src/lr8/task2/text2.txt"), "cp1251"));
-            // Переписывание информации из одного файла в другой
-//            br.readLine();
-//            bw.write(br.readLine());
-//            bw.newLine();
-//            for (int i = 1; i <= 5; i++) {
-//                System.out.println(br.read());
-//                double num = br.readLine();
-//                if (num > 0) {
-//                    bw.write(num);
-//                    bw.newLine();
-//                }
-//            }
         } catch (IOException e) {
             System.out.println("Ошибка!!!!!!!!");
         } finally {
